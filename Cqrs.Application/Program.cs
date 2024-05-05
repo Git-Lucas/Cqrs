187baad9ed3d47c2d4a34a22a47608d67a848ff7
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-05-05 17:57:49 -0300

    [CQRS] - feat: Criados projetos de aplicação e infraestrutura, e criado 'Adapter' para SQL Server com EF

---
+++ Cqrs.Application/Program.cs
@@ -0,0 +1,33 @@
+using Cqrs.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new Exception("Unable to get the connection string.");
+builder.Services.AddDbContext<EfSqlServerAdapter>(opt => 
+    opt.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+IServiceScope scope = app.Services.CreateScope();
+EfSqlServerAdapter context = scope.ServiceProvider.GetRequiredService<EfSqlServerAdapter>();
+await context.Database.MigrateAsync();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Cqrs.Infrastructure/Data/EfSqlServerAdapter.cs
@@ -0,0 +1,14 @@
+﻿using Cqrs.Domain.Companies.Entities;
+using Cqrs.Domain.Departments.Entities;
+using Cqrs.Domain.Employees.Entities;
+using Cqrs.Domain.Projects.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cqrs.Infrastructure.Data;
+public class EfSqlServerAdapter(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Project> Projects { get; set; }
+}
